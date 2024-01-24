e2278cc2b8c0ef36d905e57336e902f0be2f729d
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-24 16:51:00 +0100

    create fiber app

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/Nohty/blog/database"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/Nohty/blog/config"
+	"github.com/Nohty/blog/database"
+	"github.com/Nohty/blog/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	app := fiber.New(fiber.Config{
+		ErrorHandler: utils.ErrorHandler,
+	})
+
 	database.Connect()
+
+	if app.Listen(fmt.Sprintf(":%s", config.PORT)) != nil {
+		slog.Error("Error when starting server")
+		os.Exit(1)
+	}
 }
