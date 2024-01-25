68a195f08c5341537c6673623dbd617f632eb8ba
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 22:44:53 +0100

    add category and search functionality to BlogHandler

--- handler/web.go
+++ handler/web.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"slices"
+	"strings"
+
 	"github.com/Nohty/blog/database"
 	"github.com/Nohty/blog/model"
 	"github.com/Nohty/blog/utils"
@@ -18,6 +21,9 @@ func HomeHandler(c *fiber.Ctx) error {
 }
 
 func BlogHandler(c *fiber.Ctx) error {
+	category := c.Query("category")
+	search := c.Query("search")
+
 	var user model.User
 	if err := database.DB.First(&user, database.USER_ID).Error; err != nil {
 		return fiber.ErrInternalServerError
@@ -28,5 +34,39 @@ func BlogHandler(c *fiber.Ctx) error {
 		return fiber.ErrInternalServerError
 	}
 
-	return utils.Render(c, pages.Blog(user, posts))
+	var filteredPosts []model.BlogPost
+
+	if category != "" {
+		for _, post := range posts {
+			if post.Category == category {
+				filteredPosts = append(filteredPosts, post)
+			}
+		}
+	} else {
+		filteredPosts = posts
+	}
+
+	if search != "" {
+		var filteredSPosts []model.BlogPost
+		for _, post := range filteredPosts {
+			if strings.Contains(post.Title, search) {
+				filteredSPosts = append(filteredPosts, post)
+			}
+		}
+
+		filteredPosts = filteredSPosts
+	}
+
+	if category == "" && search == "" {
+		filteredPosts = posts
+	}
+
+	var categories []string
+	for _, post := range posts {
+		if !slices.Contains(categories, post.Category) {
+			categories = append(categories, post.Category)
+		}
+	}
+
+	return utils.Render(c, pages.Blog(user, filteredPosts, search, category, categories))
 }
