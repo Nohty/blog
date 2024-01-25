93d4dfa756e7e523da7b0bf6f018c0b17b9168f0
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 16:52:15 +0100

    commented out unused code

--- router/router.go
+++ router/router.go
@@ -3,18 +3,18 @@ package router
 import "github.com/gofiber/fiber/v2"
 
 func SetupRoutes(app *fiber.App) {
-	// Group routes
-	api := app.Group("/api")
-	web := app.Group("/")
+	// // Group routes
+	// api := app.Group("/api")
+	// web := app.Group("/")
 
-	// API routes User
-	user := api.Group("/user")
+	// // API routes User
+	// user := api.Group("/user")
 
-	// API routes Blog
-	blog := api.Group("/blog")
+	// // API routes Blog
+	// blog := api.Group("/blog")
 
-	// Web routes Blog
+	// // Web routes Blog
 	
-	// Web routes Admin
-	admin := web.Group("/admin")
+	// // Web routes Admin
+	// admin := web.Group("/admin")
 }
