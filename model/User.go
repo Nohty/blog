8f65c33fa1cdb2e2599f41ea50234e34a453c0e4
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 19:11:49 +0100

    add profession, Linkedin, and Github fields to User model

--- model/User.go
+++ model/User.go
@@ -7,5 +7,8 @@ type User struct {
 	Name       string `gorm:"not null" json:"name"`
 	Email      string `gorm:"uniqueIndex;not null" json:"email"`
 	Password   string `gorm:"not null" json:"-"`
+	Profession string `gorm:"not null" json:"profession"`
 	ImageUrl   string `gorm:"not null" json:"imag_url"`
+	Linkedin   string `gorm:"not null" json:"linkedin"`
+	Github     string `gorm:"not null" json:"github"`
 }
