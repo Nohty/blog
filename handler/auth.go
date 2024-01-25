5794ed915c8c206d91dd3be8f4a34c61b94a64f9
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 19:11:16 +0100

    add login handler for authentication

---
+++ handler/auth.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"github.com/Nohty/blog/database"
+	"github.com/Nohty/blog/middleware"
+	"github.com/Nohty/blog/model"
+	"github.com/Nohty/blog/utils"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(c *fiber.Ctx) error {
+	type LoginInput struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,ascii"`
+	}
+
+	input := new(LoginInput)
+
+	if err := utils.ParseBodyAndValidate(c, input); err != nil {
+		return err
+	}
+
+	var user model.User
+	if err := database.DB.First(&user, 0).Error; err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+	if err != nil {
+		return fiber.ErrUnauthorized
+	}
+
+	token, err := middleware.GenerateJWT(user.ID)
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	return utils.Response(c, fiber.StatusOK, token)
+}
