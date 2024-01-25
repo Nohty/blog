88cb550abdea97051ab7c00975a454bcdcbe8528
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 19:11:55 +0100

    add auth routes and user creation route

--- router/router.go
+++ router/router.go
@@ -1,20 +1,29 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Nohty/blog/handler"
+	"github.com/gofiber/fiber/v2"
+)
 
 func SetupRoutes(app *fiber.App) {
-	// // Group routes
-	// api := app.Group("/api")
-	// web := app.Group("/")
+	// Group routes
+	api := app.Group("/api")
+	web := app.Group("/")
 
-	// // API routes User
-	// user := api.Group("/user")
+	// API routes Auth
+	auth := api.Group("/auth")
+	auth.Post("/login", handler.Login)
+
+	// API routes User
+	user := api.Group("/user")
+	user.Post("/", handler.CreateUser)
 
 	// // API routes Blog
 	// blog := api.Group("/blog")
 
-	// // Web routes Blog
-	
+	// Web routes Blog
+	web.Get("/", handler.HomeHandler)
+
 	// // Web routes Admin
 	// admin := web.Group("/admin")
 }
