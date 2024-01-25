85316b2bb580087efba5dd99a1be172827b8ed77
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-25 16:52:22 +0100

    add embed package and serve static assets with embed.FS

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/Nohty/blog/config"
 	"github.com/Nohty/blog/database"
+	"github.com/Nohty/blog/middleware"
 	"github.com/Nohty/blog/router"
 	"github.com/Nohty/blog/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
+
+//go:embed assets
+var embedDirAssets embed.FS
 
 func main() {
 	app := fiber.New(fiber.Config{
@@ -20,8 +27,16 @@ func main() {
 
 	database.Connect()
 
-	app.Use(logger.New())
 	router.SetupRoutes(app)
+
+	app.Use("/assets", filesystem.New(filesystem.Config{
+		Root:       http.FS(embedDirAssets),
+		PathPrefix: "assets",
+		Browse:     true,
+	}))
+
+	app.Use(logger.New())
+	app.Use(middleware.NotFoundMiddleware)
 
 	if app.Listen(fmt.Sprintf(":%s", config.PORT)) != nil {
 		slog.Error("Error when starting server")
