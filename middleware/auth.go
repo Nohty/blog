005f58981be0f3783ea2be4c3af9be5a046198fe
Daan Hautekiet <daanhautekiet@gmail.com>
2024-01-29 21:38:11 +0100

    add ProtectedWeb middleware for web authentication

--- middleware/auth.go
+++ middleware/auth.go
@@ -34,3 +34,25 @@ func Protected(c *fiber.Ctx) error {
 
 	return fiber.ErrUnauthorized
 }
+
+func ProtectedWeb(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	if cookie == "" {
+		return c.Redirect("/admin/login")
+	}
+
+	token, err := parseJWT(cookie)
+	if err != nil {
+		return c.Redirect("/admin/login")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return c.Redirect("/admin/login")
+		}
+
+		return c.Next()
+	}
+
+	return c.Redirect("/admin/login")
+}
